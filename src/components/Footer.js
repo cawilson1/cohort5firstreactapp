f271e317375de1495a82e208f3b4983bc1f27100
cawilson1 <cawilson1@g.cofc.edu>
2020-09-21 11:03:50 -0400

    refactored header and footer into different components

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      This is a footer
+      <img
+        src="https://a.slack-edge.com/8a6a9/img/product-ui-generator/apps/twitter.png"
+        alt="twitter"
+      />
+    </div>
+  );
+}
+
+export default Footer;
